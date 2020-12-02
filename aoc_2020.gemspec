4b0e81c20cd3959ce73b77ecea27d51668f6b1df
James O'Shea <james.oshea@workday.com>
2020-12-02 10:22:44 +0000

    Day01 update

--- aoc_2020.gemspec
+++ aoc_2020.gemspec
@@ -30,6 +30,7 @@ Gem::Specification.new do |spec|
   spec.add_dependency "thor"
 
   spec.add_development_dependency 'bundler', '2.1.4'
+  spec.add_development_dependency 'gem-release'
   spec.add_development_dependency 'rake', '~> 12.0'
   spec.add_development_dependency 'rspec', '~> 3.0'
   spec.add_development_dependency 'rubocop', '1.4.2'
--- lib/aoc_2020/day_01.rb
+++ lib/aoc_2020/day_01.rb
@@ -8,23 +8,23 @@ module Aoc2020
     end
 
     def part_one
-      expense_report(2)
+      expense_report(2, 2020)
     end
 
     def part_two
-      expense_report(3)
+      expense_report(3, 2020)
     end
 
     private
 
-    def expense_report(item_count)
-      values      ||= @input.sort
-      sample_size ||= item_count
-      values.first(sample_size).combination(item_count).to_a.each { |a| return a.inject(:*) if a.inject(:+).eql?(2020) }
+    def expense_report(item_count, total)
+      values ||= @input.sort
+      sample ||= item_count
+      values.first(sample).combination(item_count).to_a.each { |a| return a.inject(:*) if a.inject(:+).eql?(total) }
       raise 'did not find an answer'
     rescue StandardError => e
-      sample_size += 1
-      retry if sample_size <= values.size
+      sample += 1
+      retry if sample <= values.size
       raise e
     end
   end
